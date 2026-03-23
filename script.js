5ebe7ccfca1bc07de57fc23c052034c012e44f23
monikada82 <damonika8208@gmail.com>
2026-03-23 20:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square, index) => {
+  square.addEventListener("mouseover", () => {
+    squares.forEach((s, i) => {
+      s.style.backgroundColor = (i === index) ? "#E6E6FA" : "#6F4E37";
+    });
+  });
+
+  square.addEventListener("mouseout", () => {
+    squares.forEach(s => {
+      s.style.backgroundColor = "#E6E6FA";
+    });
+  });
+});
